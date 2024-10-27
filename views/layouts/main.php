5783e3d2cee19a30971db15c017386e4bea8f605
pusp joshi <erpushparaj23@gmail.com>
2024-10-27 14:13:44 +0545

    changes layout design

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -17,7 +17,7 @@ use \eork\phpmvc\Application;
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php echo $this->title; ?></title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/bootstrap.css" />
 </head>
 
 <body>
